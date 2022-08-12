219320868a968a436af5ef4c846de8f2964322aa
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-08-12 11:34:16 -0300

    debit Card PIN

--- bankAccountApp/src/bankAccountApp/Checking.java
+++ bankAccountApp/src/bankAccountApp/Checking.java
@@ -11,16 +11,27 @@ public class Checking extends Account {
 	public Checking(String name, String socialSecNumber, double initDeposit) {
 		super(name, socialSecNumber, initDeposit);
 		accountNumber = "2" + accountNumber;
+		setDebitCard();
+		
 	
 	
 	}
-	
+	private void setDebitCard() {
+		debitCardNumber = (int)(Math.random() * Math.pow(10, 12));
+		debitCardPin = (int)(Math.random() * Math.pow(10, 4));
+		
+	}
 	
 	//list methods specific to the checking account
 	
 	public void showInfo() {
 		super.showInfo();
-		System.out.println("ACCOUNT TYPE: Checking ");
+		System.out.println("*********************************");
+		
+		System.out.println("Your Checking Account Features:  " +
+							"\n Debit Card Number: " +debitCardNumber+
+							"\n Debit Card PIN " + debitCardPin
+							);
 	}
 
 }
